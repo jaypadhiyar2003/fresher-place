975d8abf953210a1c7aff436259b5762990d01c9
jaypadhiyar2003 <padhiyarjay77@gmail.com>
2025-02-22 15:02:09 +0530

    docker start.sh added

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Fresher-Place</title>
     <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,100..600;1,100..600&display=swap" rel="stylesheet">
-    @vite(['resources/js/app.js','resources/css/app.css'])
+    @vite([url('resources/js/app.js'),url('resources/css/app.css')])
 </head>
 <body class="bg-black text-white font-hanken-grotesk pb-20">
     <div class="p">
