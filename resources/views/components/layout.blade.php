2cf38bf481adf001147d252c23409b23e5b846d3
jaypadhiyar2003 <padhiyarjay77@gmail.com>
2025-02-22 16:28:44 +0530

    vite assets solving

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,10 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Fresher-Place</title>
-    <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,100..600;1,100..600&display=swap" rel="stylesheet">
-    @vite('resources/css/app.css')
 </head>
-<body class="bg-black text-white font-hanken-grotesk pb-20">
+<body>
 hi
 </body>
 </html>
