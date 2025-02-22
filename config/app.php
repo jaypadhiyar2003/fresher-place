7ca29f71292b1fe07fe6b41cdbe9961db5a15e03
jaypadhiyar2003 <padhiyarjay77@gmail.com>
2025-02-22 22:04:47 +0530

    css not loading

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://fresher-place.onrender.com/public/'),
+    'url' => env('APP_URL', 'https://fresher-place.onrender.com'),
 
     /*
     |--------------------------------------------------------------------------
