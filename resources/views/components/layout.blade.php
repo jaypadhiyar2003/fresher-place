59e6dbdfe52fffeb8de2aa9e2c1579679d9978d5
jaypadhiyar2003 <padhiyarjay77@gmail.com>
2025-02-24 15:07:10 +0530

    ico error resolved

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,6 +9,9 @@
     <link rel="apple-touch-icon" sizes="180x180" href="{{asset('apple-touch-icon.png')}}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon-32x32.png')}}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon-16x16.png')}}">
+    <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}" >
+
+
     <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,100..600;1,100..600&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css','resources/js/app.js'])
 </head>
