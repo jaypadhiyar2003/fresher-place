2b70433c617d684ca22be4ee28b5ae9d378e799b
jaypadhiyar2003 <padhiyarjay77@gmail.com>
2025-04-08 12:07:35 +0530

    Add build script for Render deployment for storage issue

--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -9,6 +9,7 @@
     </a>
 
     <div class="flex-1 flex flex-col">
+
         <a href="/jobs/{{$job->id}}">
     <p href="#" class="self-start text-sm text-gray-400">{{$job->employer->name}}</p>
         <h3 class="font-bold text-xl mt-1 group-hover:text-blue-600 transition-colors duration-500">
